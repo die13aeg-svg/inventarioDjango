07d75a98a6c0095a41ea3f2c303da659587a8907
die13aeg-svg <die13.aeg@gmail.com>
2026-04-23 22:15:08 -0400

    Add 'inventario' and 'rest_framework' to INSTALLED_APPS

--- sistema_iujo/settings.py
+++ sistema_iujo/settings.py
@@ -37,7 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'inventario', 
+    'rest_framework',
 ]
+
+LOGIN_REDIRECT_URL = 'lista_productos' 
+LOGOUT_REDIRECT_URL = 'login' 
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
